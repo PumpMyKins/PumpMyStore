6926b6321e8453e817db77d8094db16852b894dd
Clem-Fern <clemfern456@gmail.com>
2019-06-27 00:07:50 +0200

    Adding rank enum

---
+++ src/main/java/fr/pumpmybstore/rank/Rank.java
@@ -0,0 +1,31 @@
+package fr.pumpmybstore.rank;
+
+public enum Rank {
+
+	TIER1(1,1,1),
+	TIER2(3,3,3),
+	TIER3(5,5,5);
+	
+	private int modification;
+	private int kitchoice;
+	private int kitrandom;
+	
+	private Rank(int modification, int kitchoice , int kitrandom) {
+		this.modification = modification;
+		this.kitchoice = kitchoice;
+		this.kitrandom = kitrandom;
+	}
+
+	public int getModification() {
+		return modification;
+	}
+
+	public int getKitchoice() {
+		return kitchoice;
+	}
+
+	public int getKitrandom() {
+		return kitrandom;
+	}
+	
+}
