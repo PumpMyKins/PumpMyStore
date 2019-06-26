b63af401b25882f6008db52963287b8e8af7d87e
Clem-Fern <clemfern456@gmail.com>
2019-06-27 00:16:09 +0200

    Adding Kit commands class

---
+++ src/main/java/fr/pumpmybstore/kit/DeleteKitCommand.java
@@ -0,0 +1,42 @@
+package fr.pumpmybstore.kit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.pumpmybstore.Main;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
+
+public class DeleteKitCommand extends Command implements TabExecutor{
+
+	private Main main;
+
+	public DeleteKitCommand(Main main) {
+		super("pumpmybstore-kit-delete", "pumpmybstore.command.rank.set");
+		this.main = main;
+	}
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+		
+		List<String> l = new ArrayList<>();
+		
+		for (ProxiedPlayer player : this.main.getProxy().getPlayers()) {
+			
+			l.add(player.getName());
+			
+		}
+		
+		return l;
+		
+	}
+
+}
---
+++ src/main/java/fr/pumpmybstore/kit/RenewKitCommand.java
@@ -0,0 +1,43 @@
+package fr.pumpmybstore.kit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.pumpmybstore.Main;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
+
+public class RenewKitCommand extends Command implements TabExecutor {
+
+	private Main main;
+
+	public RenewKitCommand(Main main) {
+		super("pumpmybstore-kit-renew", "pumpmybstore.command.rank.set");
+		this.main = main;
+	}
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		
+		
+		
+	}
+	
+	@Override
+	public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+		
+		List<String> l = new ArrayList<>();
+		
+		for (ProxiedPlayer player : this.main.getProxy().getPlayers()) {
+			
+			l.add(player.getName());
+			
+		}
+		
+		return l;
+		
+	}
+
+}
