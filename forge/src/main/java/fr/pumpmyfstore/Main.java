2ccf463968bb3f5811963a073ba65acdfc2efa72
Clem-Fern <clemfern456@gmail.com>
2019-07-04 18:34:08 +0200

    Init mod

--- forge/src/main/java/fr/pumpmyfstore/Main.java
+++ forge/src/main/java/fr/pumpmyfstore/Main.java
@@ -1,25 +1,33 @@
-package com.example.examplemod;
+package fr.pumpmyfstore;
+
+import org.apache.logging.log4j.Logger;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION, acceptableRemoteVersions = "*")
+public class Main
 {
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
+    public static SimpleNetworkWrapper network;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        network = NetworkRegistry.INSTANCE.newSimpleChannel("FTBU-RANKS");
+        network.registerMessage(MyMessage.Handler.class, MyMessage.class, 0, Side.SERVER);
+        
     }
 
     @EventHandler
