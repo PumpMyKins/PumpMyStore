87d91dbca7fda33064483192557ce5896a58147e
Clem-Fern <clemfern456@gmail.com>
2019-07-04 19:35:42 +0200

    Fix annotation mod

--- forge/src/main/java/fr/pumpmyfstore/Main.java
+++ forge/src/main/java/fr/pumpmyfstore/Main.java
@@ -14,8 +14,8 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION, acceptableRemoteVersions = "*")
 public class Main
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "pumpmyfstore";
+    public static final String NAME = "PumpMyFStore";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
