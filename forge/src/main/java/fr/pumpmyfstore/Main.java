a113608d94e30f29e6217d7a8353a7b0591d68ab
Clem-Fern <clemfern456@gmail.com>
2019-07-05 03:04:40 +0200

    Adding dependencies on Mod annotation

--- forge/src/main/java/fr/pumpmyfstore/Main.java
+++ forge/src/main/java/fr/pumpmyfstore/Main.java
@@ -18,12 +18,13 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION, acceptableRemoteVersions = "*")
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION, acceptableRemoteVersions = "*", dependencies = Main.DEP)
 public class Main
 {
     public static final String MODID = "pumpmyfstore";
     public static final String NAME = "PumpMyFStore";
     public static final String VERSION = "1.0";
+    public static final String DEP = FTBLib.THIS_DEP + ";required-after:ftbutilities";
 
     private static Logger logger;
     public static SimpleNetworkWrapper network;
